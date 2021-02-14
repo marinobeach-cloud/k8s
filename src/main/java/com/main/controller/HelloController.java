f2a8de20f5eb5640e34e7bc6124c26da787d98fe
marinobeach <Sergio>
2021-02-14 16:42:34 +0100

    try integration with sonarqube

--- src/main/java/com/main/controller/HelloController.java
+++ src/main/java/com/main/controller/HelloController.java
@@ -3,7 +3,6 @@ package com.main.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class HelloController {
 
